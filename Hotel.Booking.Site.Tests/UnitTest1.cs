b31efe14f4dbd113c2487f30df93714bea99abc0
Zaahid Hassan <zaahid.hassan187@gmail.com>
2022-09-20 18:59:01 +0200

    Add project files.

---
+++ Hotel.Booking.Site.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Hotel.Booking.Site.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
